6545116ac77430a8a0e7f45ade0a71d9abcee81a
Ramil Shaymardanov <ramilmsh@gmail.com>
2019-07-04 12:13:36 +0500

    minor fixes

--- model_gan.py
+++ model_gan.py
@@ -1,35 +1,5 @@
 import torch.nn as nn
 
-from functools import reduce
-
-
-class LambdaBase(nn.Sequential):
-    def __init__(self, fn, *args):
-        super(LambdaBase, self).__init__(*args)
-        self.lambda_func = fn
-
-    def forward_prepare(self, input):
-        output = []
-        for module in self._modules.values():
-            output.append(module(input))
-        return output if output else input
-
-
-class Lambda(LambdaBase):
-    def forward(self, input):
-        return self.lambda_func(self.forward_prepare(input))
-
-
-class LambdaMap(LambdaBase):
-    def forward(self, input):
-        return list(map(self.lambda_func, self.forward_prepare(input)))
-
-
-class LambdaReduce(LambdaBase):
-    def forward(self, input):
-        return reduce(self.lambda_func, self.forward_prepare(input))
-
-
 model = nn.Sequential(  # Sequential,
     nn.Conv2d(1, 48, (5, 5), (2, 2), (2, 2)),
     nn.ReLU(),
