c49a19b7f2e8d1d510f763cf29a1513ce71fdde3
Ramil Shaymardanov <ramilmsh@gmail.com>
2019-07-04 12:07:20 +0500

    migrating to torch 1.x

---
+++ model_gan.py
@@ -0,0 +1,83 @@
+import torch.nn as nn
+
+from functools import reduce
+
+
+class LambdaBase(nn.Sequential):
+    def __init__(self, fn, *args):
+        super(LambdaBase, self).__init__(*args)
+        self.lambda_func = fn
+
+    def forward_prepare(self, input):
+        output = []
+        for module in self._modules.values():
+            output.append(module(input))
+        return output if output else input
+
+
+class Lambda(LambdaBase):
+    def forward(self, input):
+        return self.lambda_func(self.forward_prepare(input))
+
+
+class LambdaMap(LambdaBase):
+    def forward(self, input):
+        return list(map(self.lambda_func, self.forward_prepare(input)))
+
+
+class LambdaReduce(LambdaBase):
+    def forward(self, input):
+        return reduce(self.lambda_func, self.forward_prepare(input))
+
+
+model = nn.Sequential(  # Sequential,
+    nn.Conv2d(1, 48, (5, 5), (2, 2), (2, 2)),
+    nn.ReLU(),
+    nn.Conv2d(48, 128, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(128, 128, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(128, 128, (3, 3), (2, 2), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(128, 256, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(256, 256, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(256, 256, (3, 3), (2, 2), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(256, 512, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(512, 1024, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(1024, 1024, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(1024, 1024, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(1024, 1024, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(1024, 512, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(512, 256, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.ConvTranspose2d(256, 256, (4, 4), (2, 2), (1, 1), (0, 0)),
+    nn.ReLU(),
+    nn.Conv2d(256, 256, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(256, 128, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.ConvTranspose2d(128, 128, (4, 4), (2, 2), (1, 1), (0, 0)),
+    nn.ReLU(),
+    nn.Conv2d(128, 128, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(128, 48, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.ConvTranspose2d(48, 48, (4, 4), (2, 2), (1, 1), (0, 0)),
+    nn.ReLU(),
+    nn.Conv2d(48, 24, (3, 3), (1, 1), (1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(24, 1, (3, 3), (1, 1), (1, 1)),
+    nn.Sigmoid(),
+)
+
+immean = 0.9664114577640158
+imstd = 0.0858381272736797
--- simplify.py
+++ simplify.py
@@ -1,37 +1,40 @@
 import torch
+from torch.nn import Sequential, Module
 from torchvision import transforms
 from torchvision.utils import save_image
-from torch.utils.serialization import load_lua
 
-from PIL import Image
+from PIL import Image, ImageStat
 import argparse
 
 parser = argparse.ArgumentParser(description='Sketch simplification demo.')
-parser.add_argument('--model', type=str, default='model_gan.t7', help='Model to use.')
-parser.add_argument('--img',   type=str, default='test.png',     help='Input image file.')
-parser.add_argument('--out',   type=str, default='out.png',      help='File to output.')
+parser.add_argument('--model', type=str, default='model_gan', help='Model to use.')
+parser.add_argument('--img', type=str, default='test.png', help='Input image file.')
+parser.add_argument('--out', type=str, default='out.png', help='File to output.')
 opt = parser.parse_args()
+
+model_import = __import__(opt.model, fromlist=['model', 'immean', 'imstd'])
+model = model_import.model
+immean = model_import.immean
+imstd = model_import.imstd
 
 use_cuda = torch.cuda.device_count() > 0
 
-cache  = load_lua( opt.model )
-model  = cache.model
-immean = cache.mean
-imstd  = cache.std
-model.evaluate()
+model.load_state_dict(torch.load(opt.model + ".pth"))
+model.eval()
 
-data  = Image.open( opt.img ).convert('L')
-w, h  = data.size[0], data.size[1]
-pw    = 8-(w%8) if w%8!=0 else 0
-ph    = 8-(h%8) if h%8!=0 else 0
-data  = ((transforms.ToTensor()(data)-immean)/imstd).unsqueeze(0)
-if pw!=0 or ph!=0:
-   data = torch.nn.ReplicationPad2d( (0,pw,0,ph) )( data ).data
+
+data = Image.open(opt.img).convert('L')
+w, h = data.size[0], data.size[1]
+pw = 8 - (w % 8) if w % 8 != 0 else 0
+ph = 8 - (h % 8) if h % 8 != 0 else 0
+stat = ImageStat.Stat(data)
+
+data = ((transforms.ToTensor()(data) - immean) / imstd).unsqueeze(0)
+if pw != 0 or ph != 0:
+    data = torch.nn.ReplicationPad2d((0, pw, 0, ph))(data).data
 
 if use_cuda:
-   pred = model.cuda().forward( data.cuda() ).float()
+    pred = model.cuda().forward(data.cuda()).float()
 else:
-   pred = model.forward( data )
-save_image( pred[0], opt.out )
-
-
+    pred = model.forward(data)
+save_image(pred[0], opt.out)
